88bab9953c178666f84a770cf2685e3656b768b5
kunal-das <kd.kunaldas92@gmail.com>
2023-07-27 19:00:25 +0530

    fixes #19

--- src/services/reviewService.js
+++ src/services/reviewService.js
@@ -1,16 +1,19 @@
-const reviewRepo = require('../repositories/reviewRepo');
-const productRepo = require('../repositories/productRepo');
+const reviewRepo = require("../repositories/reviewRepo");
+const productRepo = require("../repositories/productRepo");
 
 const create = async (productId, review) => {
-    // find the product with the id
-    const product = await productRepo.findById(productId);
-    // save the incoming review
-    const newReview = await reviewRepo.save(review);
-    product.reviews.push(newReview);
-    await productRepo.save(product);
-    return product;
+  // find the product with the id
+  const product = await productRepo.findByIdWithReviews(productId);
+  // save the incoming review
+  const newReview = await reviewRepo.save(review);
+  product.rating =
+    (product.rating * product.reviews.length + newReview.rating) /
+    (product.reviews.length + 1);
+  product.reviews.push(newReview);
+  await productRepo.save(product);
+  return product;
 };
 
 module.exports = {
-    create,
+  create,
 };
