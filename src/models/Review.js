c80c217dbf3671c73a9f0d1d6f07e4f6246b81c9
kunal-das <kd.kunaldas92@gmail.com>
2023-07-28 21:03:23 +0530

    added pre-hook 'deleteOne'

--- src/models/Review.js
+++ src/models/Review.js
@@ -1,17 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Product = require("./Product");
 
-const reviewSchema = new mongoose.Schema({
+const reviewSchema = new mongoose.Schema(
+  {
     rating: {
-        type: Number,
-        min: 0,
-        max: 5,
-        default: 1,
+      type: Number,
+      min: 0,
+      max: 5,
+      default: 1,
     },
     comment: {
-        type: String,
+      type: String,
     },
-}, { timestamps: true, versionKey: false });
+  },
+  { timestamps: true, versionKey: false }
+);
 
-const Review = mongoose.model('Review', reviewSchema);
+// pre-hook on remove : when review is deleted then it should be removed from product
+// and it's contribution to product's average rating should also be removed
+reviewSchema.pre(
+  "deleteOne",
+  { document: true, query: false },
+  async function (next) {
+    const product = await Product.findOne({ reviews: this._id });
+    // console.log(`product : \n ${product}`);
+    product.rating =
+      (product.rating * product.reviews.length - this.rating) /
+      (product.reviews.length - 1);
+    product.reviews = product.reviews.filter(
+      (review) => !review._id.equals(this._id)
+    );
+    product.save();
+    next();
+  }
+);
+
+const Review = mongoose.model("Review", reviewSchema);
 
 module.exports = Review;
